80fa2eac02fc21b8dc66e73d401279f0decf3baa
Barba2k2 <lorenzo.dz@hotmail.com>
2023-06-23 11:58:02 -0300

    added: Connection w/ database

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const conncetDatabase = require("./src/database/db");
 
 const userRoute = require("./src/routes/user.routes");
-
 const port = 3000;
 
+conncetDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
 
---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const conncetDatabase = () => {
+    console.log("Wait connecting to the database...");
+
+    mongoose
+        .connect(
+            "mongodb+srv://root:root@cluster0.wh8g0b1.mongodb.net/?retryWrites=true&w=majority",
+            { useNewUrlParser: true }
+        )
+        .then(() => console.log("MongoDB Atlas Connected"))
+        .catch((error) => console.log(error));
+};
+
+module.exports = conncetDatabase;
