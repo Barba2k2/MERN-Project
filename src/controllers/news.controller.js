12a147c91765cc58fb49e3cdd4fa1cc9df28ad63
Barba2k2 <lorenzo.dz@hotmail.com>
2023-06-24 12:50:21 -0300

    ADD: data paging

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,9 @@
-import { createService, findAllService } from "../services/news.service.js";
+import {
+    createService,
+    findAllService,
+    countNews,
+} from "../services/news.service.js";
+import { ObjectId } from "mongoose";
 
 const create = async (req, res) => {
     try {
@@ -14,7 +19,7 @@ const create = async (req, res) => {
             title,
             text,
             banner,
-            id: "objectidfake1",
+            user: req.userId,
         });
 
         res.send(201);
@@ -23,9 +28,59 @@ const create = async (req, res) => {
     }
 };
 
-const findAll = (req, res) => {
-    const news = [];
-    res.send(news);
+const findAll = async (req, res) => {
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+        limit = 5;
+    }
+
+    if (!offset) {
+        offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+    console.log(currentUrl);
+
+    const next = offset + limit;
+    const nextUrl =
+        next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+        previous != null
+            ? `${currentUrl}?limit=${limit}&offset=${previous}`
+            : null;
+
+    if (news.length === 0) {
+        return res
+            .status(400)
+            .send({ message: "There are no registered news" });
+    }
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: news.map((item) => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            userName: item.user.userName,
+            avatar: item.user.avatar,
+        })),
+    });
 };
 
-export default { create, findAll };
+export { create, findAll };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import userService from "../services/user.service.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const authMiddleware = (req, res, next) => {
+    try {
+        const { authorization } = req.headers;
+
+        const parts = authorization.split(" ");
+
+        const [schema, token] = parts;
+
+        if (schema !== "Bearer" || parts.length !== 2 || !authorization) {
+            return res.send(401);
+        }
+
+        jwt.verify(token, process.env.SECRET_JWT, async (error, decoded) => {
+            if (error) {
+                return res.status(401).send({ message: "Token Invalid" });
+            }
+            console.log(decoded);
+
+            const user = await userService.findByIdService(decoded.id);
+
+            if (!user || !user.id) {
+                return res.status(401).send({ message: "Invalid token!" });
+            }
+
+            req.userId = user.id;
+
+            return next();
+        });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
