cad2da3018457b39c1c056322f665265bb742add
Barba2k2 <lorenzo.dz@hotmail.com>
2023-06-24 10:09:22 -0300

    feat: create News Schema

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,22 +1,29 @@
 import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service.js";
+import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
+    const { email, password } = req.body;
+
     try {
-        const { email, password } = req.body;
         const user = await loginService(email);
 
         if (!user) {
-            return res.status(404).send({ message: "User or Password not found" });
+            return res
+                .status(404)
+                .send({ message: "User or Password not found" });
         }
 
         const passwordIsValid = bcrypt.compareSync(password, user.password);
 
         if (!passwordIsValid) {
-            return res.status(404).send({ message: "User or Password not found" });
+            return res
+                .status(404)
+                .send({ message: "User or Password not found" });
         }
 
-        res.send("Login OK!");
+        const token = generateToken(user.id);
+
+        res.send({token});
     } catch (err) {
         res.status(500).send(err.message);
     }
