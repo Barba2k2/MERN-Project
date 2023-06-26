49b2760e0377fd29518e770e3d6a120e612f4805
Barba2k2 <lorenzo.dz@hotmail.com>
2023-06-26 12:56:40 -0300

    refact: Alter swagger route to common js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import newsRoute from "./routes/news.route.js";
-import swaggerRoute from "./routes/swagger.route.js";
+import swaggerRoute from "./routes/swagger.route.cjs";
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
 conncetDatabase();
 app.use(express.json());
