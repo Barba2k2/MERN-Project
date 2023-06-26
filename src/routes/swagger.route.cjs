b87d7b0faf94466a40e0b8f1abd5e4d631f955c9
Barba2k2 <lorenzo.dz@hotmail.com>
2023-06-26 12:58:07 -0300

    refact: alter swagger route to common js

--- src/routes/swagger.route.cjs
+++ src/routes/swagger.route.cjs
@@ -6,4 +6,4 @@ const swaggerDocument = require("../swagger.json");
 router.use("/", swaggerUi.serve);
 router.get("/", swaggerUi.setup(swaggerDocument));
 
-moduele.exports = router;
+module.exports = router;
